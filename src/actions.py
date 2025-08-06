b84157ef6c86b39bb74e9f9603703099d5ebb3d0
Arnold Wikey <arnold.wikey@gmail.com>
2025-08-06 12:15:30 -0700

    syntax fix in git commit message;

--- src/actions.py
+++ src/actions.py
@@ -5,5 +5,5 @@ import subprocess
 
 def push_to_github(*args, **kwargs):
     subprocess.call(['git', 'add', f"workflows/{kwargs['workflow']}/outputs/*"])
-    subprocess.call(['git', 'commit', '-m', f"'adding outputs from {kwargs['workflow']} workflow"])
+    subprocess.call(['git', 'commit', '-m', f"adding outputs from {kwargs['workflow']} workflow"])
     subprocess.call(['git', 'push', 'origin'])
